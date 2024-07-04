36019d0999ef3942abd02dca82ffe86519c15d8c
specul8tor <azaanazam1606@gmail.com>
2024-07-04 01:41:08 -0400

    Tailwind IntelliSense

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <SafeAreaView className="flex-1 justify-center items-center">
       <Text>AmiCare</Text>
-    </View>
+    </SafeAreaView>
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <Text>AmiCare</Text>
+    // </View>
   );
 }
