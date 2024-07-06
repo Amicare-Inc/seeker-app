3522d513c306b3c5ac59cc38ae7dfeea0515f6c9
specul8tor <azaanazam1606@gmail.com>
2024-07-06 19:40:17 -0400

    Added sign in and sign up Cleaned up code

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, SafeAreaView } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from 'react'
+import ForumField from '@/components/ForumField';
+import CustomButton from '@/components/CustomButton';
+import { Link, router } from 'expo-router';
 
 const SignIn = () => {
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white" >
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="flex w-full h-full justify-center px-9">
+          <Text className="text-3xl text-black font-normal text-left mb-3">
+            Welcome Back
+          </Text>
+          < Text className="text-xs text-gray-500 font-normal text-left mb-4">
+            Let's get started by filling out the from below
+          </Text>
+          <ForumField 
+          title = "Email"
+          value = {form.email}
+          handleChangeText={(e) => setForm({...form, email: e})}
+          otherStyles="mb-4"
+          keyboardType="email-address"
+          />
+          <ForumField 
+          title = "Password"
+          value = {form.password}
+          handleChangeText={(e) => setForm({...form, password: e})}
+          otherStyles="mb-4"
+          />
+          <CustomButton 
+          title="Sign In "
+          handlePress={() => router.push("/discover")}
+          containerStyles= "mb-6"       
+          />
+          <View className="flex justify-center flex-row gap-2">
+            <Text className="text-xs text-gray-500 font-normal text-center">
+              Don't have an account?
+            </Text>
+            <Link href="/sign-up" className="text-xs font-normal text-blue-900">
+              Sign Up here
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#FFFFFF" style="dark" />
+    </SafeAreaView>
   )
 }
 
---
+++ components/ForumField.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, TextInputProps } from "react-native";
+
+
+interface ForumFieldProps extends TextInputProps{
+    title: string
+    value: string;
+    // placeholder: string
+    handleChangeText: (e:string) => void;
+    otherStyles?: string;
+}
+
+
+const ForumField: React.FC<ForumFieldProps> = ({
+  title,
+  value,
+//   placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <View className={otherStyles}>
+
+      <View className="w-full h-16 px-4 bg-gray-200 rounded-xl flex flex-row items-center">
+        <TextInput
+          className="flex-1 text-black font-normal text-base"
+          value={value}
+          placeholder={title}
+          placeholderTextColor="#4B5563"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+          {...props}
+        />
+
+        {/* {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+          </TouchableOpacity>
+        )} */}
+      </View>
+    </View>
+  );
+};
+
+export default ForumField;
