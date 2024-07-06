d1c238a9f0ae09e2ee2cd22326a9fe061f9ae001
specul8tor <azaanazam1606@gmail.com>
2024-07-06 01:29:16 -0400

    Added auth route with sign up and sign in updated start screen to have the big text Added nativewind documentation

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,21 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
-      <Text>AmiCare</Text>
+    <SafeAreaView className="h-full" >
+      {/* <Text>AmiCare</Text>
+      <Link href="/sign-in">sign in</Link> */}
+      <ScrollView contentContainerStyle = {{height: '100%'}}>
+        <View className="flex w-full h-full justify-center items-center p-4">
+          <View className="">
+            <Text className="text-5xl text-black font-thin text-center">
+              Connect with {"\n"} trusted {"\n"} personal care {"\n"} support
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-    // <View
-    //   style={{
-    //     flex: 1,
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //   }}
-    // >
-    //   <Text>AmiCare</Text>
-    // </View>
   );
 }
