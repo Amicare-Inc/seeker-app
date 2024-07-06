bd28ee4a23203127202dda124c5be809dbcd758b
specul8tor <azaanazam1606@gmail.com>
2024-07-06 15:06:02 -0400

    Added Sign in button

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,21 @@
-import { Link } from "expo-router";
-import { ScrollView, Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
+import { ScrollView, Text, View, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView className="h-full" >
-      {/* <Text>AmiCare</Text>
-      <Link href="/sign-in">sign in</Link> */}
-      <ScrollView contentContainerStyle = {{height: '100%'}}>
+    <SafeAreaView className="h-full bg-white" >
+       <ScrollView contentContainerStyle={{ height: '100%' }}>
         <View className="flex w-full h-full justify-center items-center p-4">
-          <View className="">
-            <Text className="text-5xl text-black font-thin text-center">
-              Connect with {"\n"} trusted {"\n"} personal care {"\n"} support
-            </Text>
-          </View>
+          <Text className="text-5xl text-black font-thin text-center mb-11">
+            Connect with {"\n"} trusted {"\n"} personal care {"\n"} support
+          </Text>
+          <CustomButton 
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-11"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
