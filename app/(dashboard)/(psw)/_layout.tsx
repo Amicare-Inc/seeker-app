6169923d9f448b2d9ca0665b07c6f94077d1b1c4
specul8tor <azaanazam1606@gmail.com>
2025-03-17 01:32:20 -0400

    added icons to tabs

--- app/(dashboard)/(psw)/_layout.tsx
+++ app/(dashboard)/(psw)/_layout.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Stack, Tabs } from "expo-router"
+import { Tabs } from "expo-router"
+import { Ionicons } from '@expo/vector-icons';
 
 const PswDashboardLayout = () => {
   return (
     <>
       <Tabs
-      screenOptions={{tabBarShowLabel: true}}
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: true,
+        tabBarActiveTintColor: "#008DF4",  // Active tint color
+        tabBarInactiveTintColor: "#999",    // Inactive tint color
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: React.ComponentProps<typeof Ionicons>['name'] = "home";
+          if (route.name === 'psw-home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'psw-sessions') {
+            iconName = focused ? 'time' : 'time-outline';
+          } else if (route.name === 'psw-profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
       >
         <Tabs.Screen
           name="psw-home"
