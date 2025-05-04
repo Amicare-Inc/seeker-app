3a59885f8beb045bef50356db43e3b8ec39a97c5
Dylan Li <dylanli0419@gmail.com>
2025-05-04 13:35:17 -0400

    fix: updated app/_layout.tsx for new expo version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,13 +6,10 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { store, RootState, AppDispatch } from '@/redux/store';
 import { fetchAvailableUsers } from '@/redux/userListSlice';
 import { listenToUserSessions } from '@/redux/sessionSlice';
-import { current } from '@reduxjs/toolkit';
 
 const GlobalDataLoader = () => {
 	const dispatch = useDispatch<AppDispatch>();
-	// Current user should be set after login.
 	const currentUser = useSelector((state: RootState) => state.user.userData);
-	// const sessions = useSelector((state: RootState) => state.sessions.allSessions);
 
 	useEffect(() => {
 		let unsubscribe: (() => void) | undefined;
@@ -30,41 +27,26 @@ const GlobalDataLoader = () => {
 	return null;
 };
 
-const RootLayout = () => {
+const LayoutWithProviders = () => {
 	return (
-		<Provider store={store}>
-			<SafeAreaProvider>
-				{/* GlobalDataLoader preloads global slices (user list and sessions) as soon as the user is available */}
-				<GlobalDataLoader />
-				<Stack>
-					<Stack.Screen
-						name="index"
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name="lock"
-						options={{ headerShown: false, gestureEnabled: false }}
-					/>
-					<Stack.Screen
-						name="(auth)"
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name="(dashboard)"
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name="(onboarding)"
-						options={{ headerShown: false, gestureEnabled: false }}
-					/>
-					<Stack.Screen
-						name="(chat)"
-						options={{ headerShown: false }}
-					/>
-				</Stack>
-			</SafeAreaProvider>
-		</Provider>
+		<SafeAreaProvider>
+			<GlobalDataLoader />
+			<Stack>
+				<Stack.Screen name="index" options={{ headerShown: false }} />
+				<Stack.Screen name="lock" options={{ headerShown: false, gestureEnabled: false }} />
+				<Stack.Screen name="(auth)" options={{ headerShown: false }} />
+				<Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+				<Stack.Screen name="(onboarding)" options={{ headerShown: false, gestureEnabled: false }} />
+				<Stack.Screen name="(chat)" options={{ headerShown: false }} />
+			</Stack>
+		</SafeAreaProvider>
 	);
 };
 
-export default RootLayout;
+export default function RootLayout() {
+	return (
+		<Provider store={store}>
+			<LayoutWithProviders />
+		</Provider>
+	);
+}
