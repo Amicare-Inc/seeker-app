4119a4db127892b25e90f6bab3ef478b8d3f6f8a
Dylan Li <dylanli0419@gmail.com>
2025-07-15 13:47:43 -0400

    feat: fixed android margin issues in the seeker side on the explore list

--- app/(profile)/family/add.tsx
+++ app/(profile)/family/add.tsx
@@ -21,6 +21,14 @@ const FamilyAddScreen = () => {
       email,
       isPrimary,
     });
+    
+    // If Make Primary Contact is checked, navigate to add-email
+    if (isPrimary) {
+      router.push('/family/add-email');
+    } else {
+      // Otherwise, go back to the previous screen
+      router.back();
+    }
   };
 
   const handleDelete = () => {
@@ -33,7 +41,7 @@ const FamilyAddScreen = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-neutral-100">
-      <View className="flex-row items-center px-4">
+      <View className="flex-row items-center px-4 mb-[34px]">
         <TouchableOpacity onPress={handleBackPress} className="mr-4 absolute left-4">
           <Ionicons name="chevron-back" size={24} color="black" />
         </TouchableOpacity>
@@ -44,21 +52,21 @@ const FamilyAddScreen = () => {
 
       <ScrollView className="flex-1 px-4">
         {/* Profile Picture Holder */}
-        <View className="items-center mt-8">
+        <View className="items-center mb-[40px]">
           <View className="h-[100px] w-[100px] bg-grey-9 rounded-full items-center justify-center">
             <Text className="text-grey-58">Add Photo</Text>
           </View>
         </View>
 
         {/* Input Fields */}
-        <View className="mt-8">
+        <View>
           <Text className="mb-2 text-base font-semibold">Name</Text>
           <TextInput
             value={name}
             onChangeText={setName}
             placeholder="First Name, Last Name"
-            placeholderTextColor="#A3A3A3" // grey-35
-            className="bg-white p-3.5 rounded-lg text-base mb-5"
+            placeholderTextColor="#9D9DA1"
+            className="bg-white p-3 rounded-lg text-lg mb-5 border border-grey-9"
             style={{ color: 'black' }}
           />
           <Text className="mb-2 text-base font-semibold">Phone Number</Text>
@@ -66,8 +74,8 @@ const FamilyAddScreen = () => {
             value={phoneNumber}
             onChangeText={setPhoneNumber}
             placeholder="e.g. (510) 123-4567"
-            placeholderTextColor="#A3A3A3" // grey-35
-            className="bg-white p-3.5 rounded-lg text-base mb-5"
+            placeholderTextColor="#7B7B7E" // grey-35
+            className="bg-white p-3.5 rounded-lg text-base mb-5 border border-grey-9"
             style={{ color: 'black' }}
           />
           <View className="flex-row w-full gap-3">
@@ -77,8 +85,8 @@ const FamilyAddScreen = () => {
                 value={country}
                 onChangeText={setCountry}
                 placeholder="..."
-                placeholderTextColor="#A3A3A3" // grey-35
-                className="bg-white p-3.5 rounded-lg text-base mb-5"
+                placeholderTextColor="#9D9DA1" // grey-35
+                className="bg-white p-3.5 rounded-lg text-base mb-5 border border-grey-9"
                 style={{ color: 'black' }}
               />
             </View>
@@ -88,27 +96,19 @@ const FamilyAddScreen = () => {
                 value={city}
                 onChangeText={setCity}
                 placeholder="..."
-                placeholderTextColor="#A3A3A3" // grey-35
-                className="bg-white p-3.5 rounded-lg text-base mb-5"
+                placeholderTextColor="#9D9DA1" // grey-35
+                className="bg-white p-3.5 rounded-lg text-base mb-5 border border-grey-9"
                 style={{ color: 'black' }}
               />
             </View>
+ 
           </View>
 
-          <Text className="mb-2 text-base font-semibold">Email Address (Optional)</Text>
-          <TextInput
-            value={email}
-            onChangeText={setEmail}
-            placeholder="..."
-            placeholderTextColor="#A3A3A3" // grey-35
-            className="bg-white p-3.5 rounded-lg text-base mb-7"
-            style={{ color: 'black' }}
-          />
 
           {/* Checkbox */}
           <TouchableOpacity
             onPress={() => setIsPrimary(!isPrimary)}
-            className="flex-row items-center space-x-2"
+            className="flex-row items-center space-x-2 mb-[20px]"
           >
             <Text className="text-base font-semibold">Make Primary Contact?</Text>
             <View
@@ -118,12 +118,31 @@ const FamilyAddScreen = () => {
             >
               {isPrimary && <Ionicons name="checkmark" size={16} color="white" />}
             </View>
+            <Text className="underline text-brand-blue text-xs">What's this?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            className="flex-row items-center space-x-2"
+          >
+            <Text className="text-base font-semibold">Make Care Recipient?</Text>
+            <View
+              className="h-5 w-5 rounded border flex justify-center items-center border-black"
+            >
+              <Ionicons name="checkmark" size={16} color="white" />
+            </View>
           </TouchableOpacity>
         </View>
       </ScrollView>
 
       {/* Save and Delete Buttons */}
       <View className="px-4 pb-6">
+        <View className="flex-row items-start mb-[30px]">
+            <Ionicons name="information-circle" size={28} color="#BFBFC3" className="mr-2 mt-0.5" />
+            <Text className="text-xs text-grey-49 leading-4 flex-1 ml-2">
+              By saving, you confirm this person has consented to be added and may receive session info.{' '}
+              <Text className="text-brand-blue">Privacy Policy</Text>
+            </Text>
+        </View>
         <TouchableOpacity
           onPress={handleSave}
           className="bg-grey-94 py-4 rounded-xl items-center"
